c907e0ca6aa8fccc1157b6413731991ff123f6ac
David Bardales <david.bardales17@gmail.com>
2022-08-19 17:38:12 -0400

    commit 1

--- passgen/passgen/settings.py
+++ passgen/passgen/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v4=cpw(+e@ebaxqynbft1uvz@m#@&@k6a_wfa760eoishc_3m!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://passgen3.herokuapp.com/']
 
